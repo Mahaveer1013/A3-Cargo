618799620fce69dc710b31fe41ab9fb76ba2cc5d
Mahaveer1013 <mahaveer30032005@gmail.com>
2024-12-11 22:19:30 +0530

    fix: bug fixed

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,18 +1,37 @@
-// app/api/user/route.js
-import { userSchema } from '../../../lib/validation';
-import { createUserData } from '../../../services/userService';
+// // app/api/user/route.js
+// import { userSchema } from '../../../lib/validation';
+// import { createUserData } from '../../../services/userService';
+
+// export async function POST(req) {
+//   const { name, age } = await req.json();
+
+//   // Validate data
+//   const { error } = userSchema.validate({ name, age });
+//   if (error) {
+//     return new Response(JSON.stringify({ error: error.details[0].message }), {
+//       status: 400,
+//     });
+//   }
+
+//   const result = createUserData(name, age);
+//   return new Response(JSON.stringify(result), { status: 201 });
+// }
+
 
 export async function POST(req) {
   const { name, age } = await req.json();
 
-  // Validate data
-  const { error } = userSchema.validate({ name, age });
-  if (error) {
-    return new Response(JSON.stringify({ error: error.details[0].message }), {
-      status: 400,
-    });
-  }
+  console.log({name, age});
+  
 
-  const result = createUserData(name, age);
-  return new Response(JSON.stringify(result), { status: 201 });
+  // Validate data
+  // const { error } = userSchema.validate({ name, age });
+  // if (error) {
+  //   return new Response(JSON.stringify({ name, age }), {
+  //     status: 400,
+  //   });
+  // }
+
+  // const result = createUserData(name, age);
+  return new Response(JSON.stringify({name, age}), { status: 201 });
 }
